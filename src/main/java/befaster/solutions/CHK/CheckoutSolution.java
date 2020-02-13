99b84d1b95ca914b960a536cfeebf1f6f155b42f
sbx_user1051 <sbx_user1051@169.254.15.77>
2020-02-13 22:33:19 +0000

    Thu Feb 13 22:33:19 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -35,7 +35,7 @@ public class CheckoutSolution {
         		noOfBs = noOfBs>0? noOfBs : 0;
         		total += (noOfBs/2)*45 + (noOfBs%2)*rate;
         	}else if(entry.getKey() == 'F') {
-        		total += (entry.getValue()/2)*rate +(entry.getValue()%2)*rate;
+        		total += (entry.getValue()/3)*2 + (entry.getValue()%3);
         	}
         	else {
         		total += entry.getValue() * rate;
@@ -44,4 +44,5 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
