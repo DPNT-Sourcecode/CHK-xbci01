7b31dd7f0fb918f903198f121115120688f1a43c
sbx_user1051 <sbx_user1051@169.254.15.77>
2020-02-13 22:06:10 +0000

    Thu Feb 13 22:06:10 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -11,10 +11,11 @@ public class CheckoutSolution {
         rates['B'] = 30;
         rates['C'] = 20;
         rates['D'] = 15;
+        rates['E'] = 40;
         int total = 0;
         char[] input = skus.toCharArray();
         for(char item : input) {
-        	if(item < 65 || item > 68)
+        	if(item < 65 || item > 69)
         		return -1;
         	if(skuCount.containsKey(item)) {
         		int val = skuCount.get(item) + 1;
@@ -25,7 +26,7 @@ public class CheckoutSolution {
         for(Map.Entry<Character, Integer> entry : skuCount.entrySet()) {
         	int rate = rates[entry.getKey()];
         	if(entry.getKey() == 'A') {
-        		total += (entry.getValue()/3)*130 + (entry.getValue()%3)*rate;
+        		total += (entry.getValue()/5)*200+ ((entry.getValue()%5)/3)*130 + ((entry.getValue()%5)%3)*rate;
         	}else if(entry.getKey() == 'B') {
         		total += (entry.getValue()/2)*45 + (entry.getValue()%2)*rate;
         	}else {
@@ -35,5 +36,6 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
